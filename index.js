d831a2fdfe5ad8cc797983a27b86373472ce991f
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-18 16:55:35 -0400

    added a short footer

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+var navLinks = document.querySelectorAll('.navbar a'); 
+var header = document.querySelector('.header');
+// don't judge me, judge judy
+var didJustHide = false;
+
+
+// Dropdown for Mystory
 function toggle_visibility(id){
     var item = document.getElementById(id);
     if(item.style.display == 'block'){
@@ -6,3 +14,22 @@ function toggle_visibility(id){
         item.style.display = 'block';
     }
 }
+
+//Scrolling event
+window.addEventListener('scroll', function(){
+    if(!didJustHide){header.style.display = 'block';}
+    
+    document.body.classList [
+        window.scrollY > 20 ? 'add': 'remove'
+    ]('scrolled');
+});
+
+navLinks.forEach(function(nav){
+    nav.addEventListener('click', function() {
+        header.style.display = 'none';
+        didJustHide = true;
+        setTimeout(function(){
+            didJustHide = false;
+        }, 200)
+    })
+} )
